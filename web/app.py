e51649e6b1ee79c8e3a02166794636ffa05b9da7
Nishant Trivedi <nishant@magnifi.fm>
2016-02-04 20:46:37 -0800

    Added web app config.
    Added docker config for nginx. Added Dockerfile to build
    a python webapp. Requirements for flask and pymongo. Also
    added a sample welcome page.

---
+++ web/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template('index.html')
+
+@app.route("/v1/journals", methods=['GET'])
+def list_journals():
+  return "This method should list journals"
+
+@app.route("/v1/journals", methods=['POST'])
+def add_journal():
+  return "This method should add a journal"
+
+@app.route("/v1/journals/temp", methods=['GET'])
+def display_journal():
+  return "This method should display a journal"
+
+@app.route("/v1/journals/temp", methods=['PUT'])
+def update_journal():
+  return "This method should update a journal"
+
+@app.route("/v1/journals/temp", methods=['DELETE'])
+def delete_journal():
+  return "This method should delete a journal"
+
+if __name__ == "__main__":
+  app.run()
